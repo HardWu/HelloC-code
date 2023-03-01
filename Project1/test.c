6fec676160e49d28b7bf99dc79ee1f7a6145d191
Windows <Windows@DESKTOP-FUBH3JG>
2023-03-01 20:43:40 +0800

    添加项目文件。

---
+++ Project1/test.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int x, y, z;
+	int sum;
+	printf("ÊäÈëÈı¸öÊä x, y, z:");
+	scanf("%d, %d, %d", &x, &y, &z);
+	sum = x+y+z;
+	printf(" sum=%d\n", sum);
+	return 0;
+}
