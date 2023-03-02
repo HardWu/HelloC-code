69b8c7dae4c2068cd5484eea83d6ee19897c5449
HardWu <126678783+HardWu@users.noreply.github.com>
2023-03-02 16:16:49 +0800

    Add files via upload

--- Project1/test.c
+++ Project1/test.c
@@ -1,12 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() {
-	int x, y, z;
-	int sum;
-	printf("输入三个输 x, y, z:");
-	scanf("%d, %d, %d", &x, &y, &z);
-	sum = x+y+z;
-	printf(" sum=%d\n", sum);
-	return 0;
+float average(float x, float y, float z) {
+	float avg;
+	avg = (x + y + z) / 3;
+	return (avg);
+}
+int MAX(int num1, int num2) {
+	if (num1>num2)
+	{
+		return num1;
+	}
+	else
+	{
+		return num2;
+	}
+}
+main() {
+	//int x, y, z;
+	//int sum;
+	//printf("输入三个输 x, y, z:");7 
+	//scanf("%d, %d, %d", &x, &y, &z);
+	//sum = x+y+z;
+	//printf(" sum=%d\n", sum);
+	float a, b, c, ave;
+	a = 3.1f; b = 3.2f; c = 3.3f;
+	ave = average(a,b,c);
+	printf("average=%f\n", ave);
+	int n1, n2, max;
+	printf("请输入两个数 n1,n2:\n");
+	scanf("%d, %d", &n1, &n2);
+	max = MAX(n1, n2);
+	printf("MAX=%d\n", max);
 }
